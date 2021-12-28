d2a7bb9108faf7eb9df3bc90bd501cc50a8456cb
anuragdandge <41123540+anuragdandge@users.noreply.github.com>
2021-12-28 20:23:13 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,11 @@ import 'package:itty/pages/login.dart';
 import 'package:itty/pages/notes.dart';
 import 'package:itty/pages/links.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'splash.dart';
+import 'pages/splash.dart';
 
-void main() {
+Future main() async {
+
+  
   runApp(const MyApp());
 }
 
@@ -19,79 +21,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  PageController _controller = PageController();
-  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AnimatedSplashScreen(
-        splash: Splash(),
-        nextScreen: Scaffold(
-          body: Container(
-            child: PageView(
-              controller: _controller,
-              onPageChanged: (index) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-              },
-              children: const [Home(), Login(), Notes(), Links()],
-            ),
-          ),
-          bottomNavigationBar: BottomNavyBar(
-            selectedIndex: _selectedIndex,
-            onItemSelected: (index) {
-              setState(() {
-                _selectedIndex = index;
-                _controller.animateToPage(index,
-                    duration: Duration(milliseconds: 300), curve: Curves.ease);
-              });
-            },
-            items: [
-              BottomNavyBarItem(
-                  icon: Icon(Icons.apps),
-                  title: Text("Home"),
-                  activeColor: Colors.red),
-              BottomNavyBarItem(
-                  icon: Icon(Icons.people),
-                  title: Text("Users"),
-                  activeColor: Colors.purpleAccent),
-              BottomNavyBarItem(
-                  icon: Icon(Icons.message),
-                  title: Text("Message"),
-                  activeColor: Colors.pink),
-              BottomNavyBarItem(
-                  icon: Icon(Icons.settings),
-                  title: Text("Settings"),
-                  activeColor: Colors.purpleAccent),
-            ],
-          ),
-        ),
-        backgroundColor: Colors.blueAccent,
-      ),
-    );
-  }
-
-  _buildPage({IconData? icon, String? title, Color? color}) {
-    return Container(
-      color: color,
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(
-              icon,
-              size: 200,
-              color: Colors.white,
-            ),
-            Text(
-              title!,
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            )
-          ],
-        ),
-      ),
-    );
+        home: AnimatedSplashScreen(
+      splash: Icons.home,
+      nextScreen: const Home(),
+      duration: 700,
+    ));
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:itty/pages/login.dart';
+import 'package:itty/pages/notes.dart';
+import 'package:itty/pages/links.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,11 +13,75 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  PageController _controller = PageController();
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          child: PageView(
+            controller: _controller,
+            onPageChanged: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            children: const [Login(), Notes(), Links()],
+          ),
+        ),
+        bottomNavigationBar: BottomNavyBar(
+          selectedIndex: _selectedIndex,
+          onItemSelected: (index) {
+            setState(() {
+              _selectedIndex = index;
+              _controller.animateToPage(index,
+                  duration: Duration(milliseconds: 300), curve: Curves.ease);
+            });
+          },
+          items: [
+            // BottomNavyBarItem(
+            //   icon: Icon(Icons.apps),
+            //   title: Text('Home'),
+            //   activeColor: Colors.red,
+            // ),
+            BottomNavyBarItem(
+                icon: Icon(Icons.people),
+                title: Text('Users'),
+                activeColor: Colors.purpleAccent),
+            BottomNavyBarItem(
+                icon: Icon(Icons.message),
+                title: Text('Messages'),
+                activeColor: Colors.pink),
+            BottomNavyBarItem(
+                icon: Icon(Icons.settings),
+                title: Text('Settings'),
+                activeColor: Colors.blue),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _buildPage({IconData? icon, String? title, Color? color}) {
     return Container(
+      color: color,
       child: Center(
-        child: Text("Home"),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon,
+              size: 200,
+              color: Colors.white,
+            ),
+            Text(
+              title!,
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            )
+          ],
+        ),
       ),
     );
   }
